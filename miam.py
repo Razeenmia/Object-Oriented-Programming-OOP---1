4ec7d277df103bf2f71465d3c9c7ecf8d9f41f68
Razeenmia <rashaad.mia@gmial.com>
2024-10-21 19:49:23 +0200

    thuiojyop

---
+++ miam.py
@@ -0,0 +1,13 @@
+class computer:
+    def __init__(self,case,motherbord,thermelpaste,cpu,coolingsistem,memory,gpu,greficscard):
+        self.case=case
+  self.case=case
+  self.motherbord=motherbord
+  self.thermelpaste=thermelpaste
+  self.cpu=cpu
+  self.coolingsistem=coolingsistem
+  self.memory=memory
+  self.greficscard=greficscard
+    pass
+class student:
+    pass
